046d3593ddd9c009011b362d6995c56b40b83bfb
Raphael Paiva <raphaelpaivaj@gmail.com>
2024-04-02 11:17:17 -0700

    FInalizando alteracoes

---
+++ assets/js/api.js
@@ -0,0 +1,6 @@
+async function fetchProfileData() {
+  const url =
+    "https://raw.githubusercontent.com/raphaelskrr/portfolio-js/main/data/profile.json";
+  const fetching = await fetch(url);
+  return await fetching.json();
+}
